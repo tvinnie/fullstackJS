e52b143428bb99c0dc64d46e8577150790be021b
tvinnie <tvinnie77@gmail.com>
2024-07-22 11:55:50 +0300

    working with accessTokens and middware

--- websitebackend/server/server.js
+++ websitebackend/server/server.js
@@ -35,20 +35,6 @@ boot(app, __dirname, function(err) {
 
 console.log('Models in the system:',Object.keys(app.models));
 
-app.models.user.find((err,result)=>{
-  if(result.length === 0) {
-    const user = {
-      email:'vin@vin.com',
-      username: 'vin',
-      password:'******'
-    };
-
-    app.models.user.create(user,(err,result) =>{
-      console.log('Tried to create user', err, result);
-    })
-  }
-})
-
 app.models.user.afterRemote('create',(ctx,user,next) =>{
   console.log("New User is: ", user);
   app.models.Profile.create({
