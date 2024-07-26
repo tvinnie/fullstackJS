9ac1ef4ab4510e212b51714d3684e2ae3cd138b4
tvinnie <tvinnie77@gmail.com>
2024-07-26 09:01:07 +0300

    configure acls

--- websitebackend/server/server.js
+++ websitebackend/server/server.js
@@ -10,9 +10,9 @@ const boot = require('loopback-boot');
 
 const app = module.exports = loopback();
 
-app.start = function() {
+app.start = function () {
   // start the web server
-  return app.listen(function() {
+  return app.listen(function () {
     app.emit('started');
     const baseUrl = app.get('url').replace(/\/$/, '');
     console.log('Web server listening at: %s', baseUrl);
@@ -25,7 +25,7 @@ app.start = function() {
 
 // Bootstrap the application, configure models, datasources and middleware.
 // Sub-apps like REST API are mounted via boot scripts.
-boot(app, __dirname, function(err) {
+boot(app, __dirname, function (err) {
   if (err) throw err;
 
   // start the server if `$ node server.js`
@@ -33,21 +33,46 @@ boot(app, __dirname, function(err) {
     app.start();
 });
 
-console.log('Models in the system:',Object.keys(app.models));
+console.log('Models in the system:', Object.keys(app.models));
 
-app.models.user.afterRemote('create',(ctx,user,next) =>{
+app.models.user.afterRemote('create', (ctx, user, next) => {
   console.log("New User is: ", user);
   app.models.Profile.create({
-    first_name:user.username,
+    first_name: user.username,
     created_at: new Date(),
     userId: user.id
-  }, (err, result)=> {
-    if(!err && result){
+  }, (err, result) => {
+    if (!err && result) {
       console.log("Created new Profile ", result);
     } else {
       console.log("There is an Error ", err)
     }
     next();
   })
-
 });
+
+// creating a default user: Admin
+app.models.Role.find({ where: { name: 'admin' } }, (err, role) => {
+  if (!err && role) {
+    console.log('No Error, Role is:', role)
+    if (role.length === 0) {
+      app.models.Role.create({
+        name: 'admin',
+      }, (err2, result) => {
+        if (!err2 && result) {
+          app.models.user.findOne((userError, user) => {
+            if (!userError && user) {
+              result.principals.create({
+                principalType: app.models.RoleMapping.USER,
+                principalId: user.id
+              }, (err3, principal)=>{
+                console.log('Created Principal', err3, principal);
+              })
+            }
+          })
+        }
+      })
+    }
+  }
+})
+
