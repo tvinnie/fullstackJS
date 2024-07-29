26c7ef3d6080b87f41c74a9f851a12d442d4fc69
tvinnie <tvinnie77@gmail.com>
2024-07-29 09:38:17 +0300

    working with models

--- websitebackend/server/server.js
+++ websitebackend/server/server.js
@@ -76,3 +76,15 @@ app.models.Role.find({ where: { name: 'admin' } }, (err, role) => {
   }
 })
 
+// check if editor role exists
+app.models.Role.find({where: {name:'editor'}}, (err,roles)=>{
+  if(!err && roles){
+    if(roles.length === 0){
+      app.models.Role.create({
+        name:'editor',
+      },(creationErr, result) =>{
+        console.log(creationErr, result);
+      })
+    }
+  }
+})
